d7e9273d8d9ecf251ec511e70caf34c8ce5d489c
surec <sureshchandru2003@gmail.com>
2021-09-05 18:40:31 +0100

    Added slide show presentation.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 
 
-function copyToClipboard(text)
-{
+function copyToClipboard(text) {
 	navigator.clipboard.writeText(text);
 }
 
+
 // called when document is ready
 $(function() {
+	$("#slide-show-container > div:gt(0)").hide();
 
+	setInterval(function() { 
+		$('#slide-show-container > div:first')
+			.hide()
+			.next()
+			.fadeIn(1000)
+			.end()
+			.appendTo('#slide-show-container');
+	}, 5000);
 })
